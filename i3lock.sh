253b61bdfd42a662ce04159f2e22efd9d0e1ed7b
Gerard Alcorlo Bofill <gerard.alcorlo@edreamsodigeo.com>
2017-09-17 22:14:05 +0200

    Added conky and update monitor_manager

--- i3lock.sh
+++ i3lock.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+set -x
+exec >> /tmp/trassa
+exec 2>&1
+
 if [ $(/usr/bin/pgrep -c i3lock\.sh$) -gt 1 ]; then
+   date >> /tmp/a
    exit 0
 fi
  
@@ -22,10 +27,17 @@ convert "$tmpbg" -scale 10% -scale 1000% -spread 6 "$tmpbg"
 #sleep 30; /usr/bin/pgrep -f i3lock\.sh$ && /usr/bin/xset dpms force off
 
 revert() {
+  echo `date` "deshabilito l'estalvi de pantalla"
   xset dpms 0 0 0
 }
+
 trap revert SIGHUP SIGINT SIGTERM
-xset +dpms dpms 30 30 30
+xset +dpms dpms 5 6 7
+
+echo "$(date) llanço el screenlok" > /tmp/trassa
 i3lock -n -t -e -f -i "$tmpbg"
 revert
+
+
+echo "$(date) fi del script" >> /tmp/trassa
 
